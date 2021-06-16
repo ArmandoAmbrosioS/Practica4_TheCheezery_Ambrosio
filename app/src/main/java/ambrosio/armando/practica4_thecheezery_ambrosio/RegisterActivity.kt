b89e6d86d216b633e3c8d12bccf780ede3854160
ArmandoAmbrosioS <armando.ambrosiosoto@gmail.com>
2021-06-15 22:34:19 -0700

    Finished

---
+++ app/src/main/java/ambrosio/armando/practica4_thecheezery_ambrosio/RegisterActivity.kt
@@ -0,0 +1,11 @@
+package ambrosio.armando.practica4_thecheezery_ambrosio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
